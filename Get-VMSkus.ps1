5f0f0ba4ad050fee34dc73c5dbd37f175d0e3009
hspc pc <hspc pc>
2017-02-04 08:43:34 +1100

    Script to get valid skus

---
+++ Get-VMSkus.ps1
@@ -0,0 +1,6 @@
+﻿Login-AzureRmAccount
+
+Get-AzureRmLocation
+Get-AzureRmVMImagePublisher -Location australiaeast | Where-Object {$_.PublisherName -like "microsoft*"}
+Get-AzureRmVMImageOffer -Location australiaeast -PublisherName MicrosoftWindowsServer
+Get-AzureRmVMImageSku -Location australiaeast -PublisherName MicrosoftWindowsServer -Offer WindowsServer
